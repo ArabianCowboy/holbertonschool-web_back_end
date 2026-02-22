dd510a1b41970462353c1a7bacf290234900ebe0
Arabiancowboy <midoxp@gmail.com>
2026-02-22 15:37:54 +0300

    refactor: clean up concat function and remove unused test code
    Remove duplicate function definition and inline test code to
    improve code clarity.

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -5,14 +5,5 @@
 
 def concat(str1: str, str2: str) -> str:
 
-    """concat two strings"""
-    return str1 + str2
-
-str1 = "egg"
-str2 = "salad"
-def concat(str1: str, str2: str) -> str:
     """Return the concatenation of two string values."""
     return str1 + str2
-
-print(concat(str1, str2) == "{}{}".format(str1, str2))
-print(concat.__annotations__)
